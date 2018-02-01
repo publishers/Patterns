be97ffd97dbb34a231e01c315eede0b1d7602698
Serhii Malykhin <serhii_malykhin@epam.com>
2018-02-01 13:32:33 +0200

    [state] pattern. Example with gum machine.

---
+++ src/com/patterns/state/DemoRunState.java
@@ -0,0 +1,40 @@
+package com.patterns.state;
+
+import com.patterns.state.gum.GumballMachine;
+
+public class DemoRunState {
+  public static void main(String[] args) {
+    GumballMachine gumballMachine = new GumballMachine(5);
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.ejectQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.ejectQuarter();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+  }
+}
